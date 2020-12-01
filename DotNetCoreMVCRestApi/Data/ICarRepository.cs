058dcd33cfb23865a4c91ae43cc006011ffc0c90
steve <jedi1014@comcast.net>
2020-12-01 09:43:57 -0500

    Add project files.

---
+++ DotNetCoreMVCRestApi/Data/ICarRepository.cs
@@ -0,0 +1,11 @@
+﻿using DotNetCoreMVCRestApi.Models;
+using System.Collections.Generic;
+
+namespace DotNetCoreMVCRestApi.Data
+{
+    public interface ICarRepository
+    {
+        List<Car> GetAllCars(); // Read
+        Car GetCarById(int id); // Read
+    }
+}
---
+++ DotNetCoreMVCRestApi/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVCRestApi.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Color { get; set; }
+        public string VIN { get; set; }
+    }
+}
